764193d498952de2e0a0f60b459ca2b49abb9341
AlexOfTheWired <Alexanderwagner.0001@Gmail.com>
2018-11-13 00:55:16 -0800

    Add files via upload

--- overrideColorRGB.py
+++ overrideColorRGB.py
@@ -1,14 +1,30 @@
 import maya.cmds as mc
 
 def overrideColorTool(R=0.0,G=0.0,B=0.0):
-    loc_selection_list = mc.ls(sl=True)
-    print(loc_selection_list)
-    rGB = [R, G, B]
-    for loc in loc_selection_list:
-        loc_shape_node = mc.listRelatives(loc, shapes=True)[0]
-        print(loc_shape_node)
-        mc.setAttr('%s.overrideEnabled'%(loc_shape_node), True)
-        mc.setAttr('%s.overrideRGBColors'%(loc_shape_node), True)
-        mc.setAttr('%s.overrideColorRGB'%(loc_shape_node), R, G, B)
+    """
+    Tool for overriding color of each control or locator and its descendent shape nodes.
+    """
+    
+    # Assign selected objects to list
+    selection_list = mc.ls(sl=True)
+    
+    for loc in xrange(len(selection_list)):
+        # Get list of shape nodes for each item in selection_list.
+        loc_shape_nodes = mc.listRelatives(selection_list[loc], shapes=True, )
+        root_shape_name = '%s'%(selection_list[loc])
+        for shape in xrange(len(loc_shape_nodes)):
+            # Iterate through each shape node in list rename to have unique name.
+            idx = (shape + 1)
+            child_shape_name = root_shape_name.replace('_01','_shape%s'%(idx))
+            mc.rename(loc_shape_nodes[shape],child_shape_name)
+            # Try setting attributes to selected RGB values.
+            try:
+                mc.setAttr('%s.overrideEnabled'%(child_shape_name), True)
+                mc.setAttr('%s.overrideRGBColors'%(child_shape_name), True)
+                mc.setAttr('%s.overrideColorRGB'%(child_shape_name), R, G, B)
+            except:
+                mc.error('More that one object shares a name. Each Shape Node must have unique name.')
 
-overrideColorTool(R=1.0,G=0.15,B=0.0)
+overrideColorTool(R=0.05,G=0.0,B=0.9) # Left side Color
+overrideColorTool(R=0.9,G=0.0,B=0.1) # Right side Color
+overrideColorTool(R=1.0,G=0.15,B=0.0) # Center Color
